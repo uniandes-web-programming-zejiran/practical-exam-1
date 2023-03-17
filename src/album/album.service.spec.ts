0cd206cb344b4c8aa736d3b1b9db6c27ece412fc
Juan Alegría <j.alegria@uniandes.edu.co>
2023-03-17 09:19:37 -0500

    Test: init test

--- src/album/album.service.spec.ts
+++ src/album/album.service.spec.ts
@@ -1,4 +1,8 @@
+import { faker } from '@faker-js/faker';
 import { Test, TestingModule } from '@nestjs/testing';
+import { PerformerEntity } from 'src/performer/performer.entity';
+import { TrackEntity } from 'src/track/track.entity';
+import { AlbumEntity } from './album.entity';
 import { AlbumService } from './album.service';
 
 describe('AlbumService', () => {
@@ -14,5 +18,20 @@ describe('AlbumService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('create should return an album', async () => {
+    const album: AlbumEntity = {
+      id: '',
+      name: faker.lorem.sentence(),
+      cover: faker.lorem.sentence(),
+      releaseDate: faker.date.between(
+        '2020-01-01T00:00:00.000Z',
+        '2030-01-01T00:00:00.000Z',
+      ),
+      description: faker.lorem.sentence(),
+      tracks: [new TrackEntity()],
+      performers: [new PerformerEntity()],
+    };
   });
 });
